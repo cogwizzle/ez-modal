e415c1df14c22c0bbeed45c7e8bcdd8898642a31
Joe Fehrman <joseph.fehrman@gmail.com>
2019-04-17 22:10:15 -0400

    Swapping styles to be based on tailwind classes.

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,11 +1,16 @@
 import { Component, Prop, Method } from "@stencil/core";
+import classnames from 'classnames';
 
 @Component({
   tag: 'ez-modal',
-  styleUrl: './modal.css',
+  styleUrls: [
+    './modal.css',
+    '../../node_modules/animate.css/animate.css'
+  ],
   scoped: true,
 })
 export class Modal {
+  firstRender: false;
   @Prop({
     reflectToAttr: true,
     mutable: true,
@@ -24,15 +29,40 @@ export class Modal {
 
   render() {
     return ([
-      <div class="backdrop" onClick={this.close.bind(this)} />,
-      <div class="ez-modal">
-        <header>
+      <div
+        class={
+          classnames(
+            'backdrop',
+            'fixed top left w-full h-screen bg-black z-10',
+            {
+              'opacity-75 pointer-events-auto': this.isOpen,
+              'opacity-0 pointer-events-none': !this.isOpen
+            }
+          )
+        }
+        onClick={this.close.bind(this)}
+      />,
+      <div class={
+        classnames(
+          'ez-modal',
+          'fixed w-screen md--w-1/2 pin-t z-20 bg-white md--rounded mt-0 md--mt-32 p-8 h-screen md--h-auto md--max-h-1/2vh animated overflow-hidden flex flex-col',
+          {
+            'opacity-100 pointer-events-auto': this.isOpen,
+            'opacity-0 pointer-events-none fadeOutUp': !this.isOpen,
+            'fadeInDown': !this.firstRender && this.isOpen,
+          }
+        )
+      }>
+        <header class="relative">
           <slot name="title" />
-          <button onClick={this.close.bind(this)} class="ez-modal__close-button">X</button>
+          <button
+            onClick={this.close.bind(this)}
+            class="absolute pin-t pin-r mr-2 font-sans text-2xl cursor-pointer border-none bg-white focus--outline-none"
+          >X</button>
         </header>
-        <main>
+        <div class="relative block overflow-y-auto">
           <slot />
-        </main>
+        </div>
       </div>
     ]);
   }
