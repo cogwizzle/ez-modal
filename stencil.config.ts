29b5b9b4e9a2a6443eaebc7546e891d1534af3b3
Joe Fehrman <joseph.fehrman@gmail.com>
2019-04-17 22:09:44 -0400

    Creating tailwind config and configuring postcss.

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,7 @@
 import { Config } from '@stencil/core';
+import { postcss } from '@stencil/postcss';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
 
 export const config: Config = {
   namespace: 'ez-modal',
@@ -9,5 +12,12 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null // disable service workers
     }
+  ],
+  plugins: [
+    postcss({
+      plugins: [
+        tailwindcss('./tailwind.js')
+      ]
+    })
   ]
 };
